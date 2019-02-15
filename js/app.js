f09a492b2c8a5093e693150dd2b8b8e378d95eda
Robin Savage <robin.savage@protonmail.com>
2019-02-15 14:09:17 -0600

    First changes
    Bugs to be fixed: List not being made.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
-//app
+(function() {
+
+  var viewModel = {
+    locales: [
+      {title: 'Secret Beach', location: {lat: 26.871856, lng: -80.049845}},
+      {title: 'Dockside Promenade', location: {lat: 26.946291, lng: -80.082824}},
+      {title: 'Sunnyside Urban Study Spot', location: {lat: 26.713548, lng: -80.055889}},
+      {title: 'Fun-N-Fellowship Locale', location: {lat: 26.930736, lng: -80.123895}},
+      {title: 'Midweek Gathering', location: {lat: 26.952620, lng: -80.138577}}
+    ] // Hardcoded locations but no markers within viewModel.
+  }; // viewModel
+  ko.applyBindings(viewModel);
+
+});
