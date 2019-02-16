6cf4a0b6d94dc46f7d1380ed1513233ca0aada3c
Robin Savage <robin.savage@protonmail.com>
2019-02-16 00:32:11 -0600

    Promises in progress

--- js/app.js
+++ js/app.js
@@ -1,28 +1,18 @@
 $(function() {
+  const newArray = []
 
-  var viewModel = {
-    currentlocale: null,
-    locales: [
-      {title: 'Secret Beach', location: {lat: 26.871856, lng: -80.049845}},
-      {title: 'Dockside Promenade', location: {lat: 26.946291, lng: -80.082824}},
-      {title: 'Sunnyside Urban Study Spot', location: {lat: 26.713548, lng: -80.055889}},
-      {title: 'Fun-N-Fellowship Locale', location: {lat: 26.930736, lng: -80.123895}},
-      {title: 'Midweek Gathering', location: {lat: 26.952620, lng: -80.138577}}
-    ], // locales
-  }; // viewModel
+  initMap().then(function(value) {
+    // $.each(window.markers, function(index) {
+    newArray.push(value);
+  });
 
-  var convert = {
-    init: function() {
-      viewModel.currentlocale = viewModel.locales[0];
-    },
-    
-  };
+console.log(newArray)
+//   var viewModel = {
+//var timeoutI
 
-
-
-
-
-
-  ko.applyBindings(viewModel);
-
+//     })
+// };
+//
+// ko.applyBindings(viewModel);
+// console.log(newArray);
 });
