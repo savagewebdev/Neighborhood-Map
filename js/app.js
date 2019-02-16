e8a607681351a3393781c273a875eb843aab5f90
Robin Savage <robin.savage@protonmail.com>
2019-02-15 19:43:04 -0600

    Working map w/markers
    To-do: Filter, UI details.

--- js/app.js
+++ js/app.js
@@ -1,22 +1,28 @@
 $(function() {
 
   var viewModel = {
+    currentlocale: null,
     locales: [
       {title: 'Secret Beach', location: {lat: 26.871856, lng: -80.049845}},
       {title: 'Dockside Promenade', location: {lat: 26.946291, lng: -80.082824}},
       {title: 'Sunnyside Urban Study Spot', location: {lat: 26.713548, lng: -80.055889}},
       {title: 'Fun-N-Fellowship Locale', location: {lat: 26.930736, lng: -80.123895}},
       {title: 'Midweek Gathering', location: {lat: 26.952620, lng: -80.138577}}
-    ], // Hardcoded locations but no markers within viewModel.
+    ], // locales
+  }; // viewModel
 
-    };
+  var convert = {
+    init: function() {
+      viewModel.currentlocale = viewModel.locales[0];
+    },
+    
+  };
 
 
 
-      // var marker = new google.maps.Marker({position: latLng, map: map});
 
 
-   // viewModel
+
   ko.applyBindings(viewModel);
 
 });
