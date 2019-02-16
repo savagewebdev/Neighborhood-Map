ba004832504967ecf947b9dd0310cdabba49a8ea
Robin Savage <robin.savage@protonmail.com>
2019-02-16 17:52:55 -0600

    Beta: Dropdown Menu integration
    Installation of hamburger menu css as well as integration of dropdown menu code.

--- js/app.js
+++ js/app.js
@@ -7,6 +7,27 @@ $(window).on('load', function () { // Google's "async defer" makes this delay ne
     });
     this.newArray = newArr;
     console.log(newArr)
+
+    function myFunction() {
+      document.getElementById("myDropdown").classList.toggle("show");
+    } // Shows the dropdown list.
+
+    function filterFunction() {
+      var input, filter, ul, li, a, i;
+      input = document.getElementById("myInput");
+      filter = input.value.toUpperCase();
+      div = document.getElementById("myDropdown");
+      a = div.getElementsByTagName("a");
+      for (i = 0; i < a.length; i++) {
+        txtValue = a[i].textContent || a[i].innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          a[i].style.display = "";
+        } else {
+          a[i].style.display = "none";
+        }
+      }
+    } // Filters the display of list items.
+
   }
   ko.applyBindings(new viewModel());
 });
