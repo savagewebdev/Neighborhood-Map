b5262e517f7d8d93e5067a6f5f21296eea529c7f
Robin Savage <robin.savage@protonmail.com>
2019-02-17 01:25:40 -0600

    JSON deleted, array pushed

--- js/data.js
+++ js/data.js
@@ -1,5 +1,10 @@
-(function data() {
-  const stashStorage = JSON.parse(localStorage.locations);
-  locations.push(stashStorage);
-  localStorage.clear();
-}());
+function data() {
+  const stashStorage = [
+        {title: 'Peirless Beach', location: {lat: 26.871856, lng: -80.049845}},
+        {title: 'Jettys\'s Waterfront Restaurant', location: {lat: 26.946291, lng: -80.082824}},
+        {title: 'Subculture Coffee, Climatis Street', location: {lat: 26.713548, lng: -80.055889}},
+        {title: 'Generation Church, Jupiter', location: {lat: 26.930736, lng: -80.123895}},
+        {title: 'Jupiter Inlet Lighthouse & Museum', location: {lat: 26.9479905, lng: -80.0859873}}
+      ];
+  return locations.push(stashStorage);
+};
