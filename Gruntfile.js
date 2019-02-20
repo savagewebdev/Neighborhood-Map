61dcf46511edbc95e0b2da2a363075530640b9a8
Robin Savage <robin.savage@protonmail.com>
2019-02-19 22:02:26 -0600

    Grunt serve installed

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'js/app.js',
+        dest: 'js/app.min.js'
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-available-tasks');
+  grunt.loadNpmTasks('grunt-serve');
+
+  // Default task(s).
+  grunt.registerTask('default', ['grunt-serve']);
+
+};
